e00466a1720caa121d6a4d8ca400bb97e136fc85
Alixy Mizuki <86276165+Alixy-Mizuki@users.noreply.github.com>
2021-12-28 23:55:19 +0700

    Update handler.js

--- ext/handler.js
+++ ext/handler.js
@@ -1,4 +1,5 @@
 const escapeRegex = (str) => { return str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&'); }
+const { oneLine, stripIndents } = require('common-tags');
 
 class Handler {
 	constructor(client, commands) {
@@ -13,7 +14,7 @@ class Handler {
 	async handleMessage(stat) {
     if(stat == false) return;
     
-    this.client.on('messageCreate', message => 
+    this.client.on('messageCreate', async (message) => 
     { 
       if (message.author.bot || (message.channel.type === 'dm') || (message.type === 'THREAD_CREATED') ) return;
       if(!message.content.startsWith(this.client.commandPrefix)) return;
@@ -21,20 +22,83 @@ class Handler {
       let CMD = args[0]
       args.shift();
 
-      let COMMAND = this.client.commands.get(CMD) || this.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(CMD));
+      let command = this.client.commands.get(CMD) || this.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(CMD));
 
+      // v13 Channel Type
+    let CT = {
+      'DM': 'dm',
+      'GROUP_DM': 'group dm'
+    }
 
-      // console.log(COMMAND, COMMAND.name)
+    let argsCollect = [];
 
-      if(COMMAND) {
-        COMMAND.run({ message, args })
+    if(command.guildOnly) {
+      if(Boolean(CT[message.channel.type])) return message.reply(`The \`${command.name}\` command must be used in a server channel.`);
+    }
+
+		if(command.nsfw) return message.reply(`The \`${command.name}\` command can only be used in NSFW channels.`);
+
+    if((command.argsRequired == true) && !args.length) {
+      if(command.usage.length > 1) {
+        let Arguments = '';
+        let Use = `${this.client.commandPrefix}${command.name}`
+
+        Arguments += `Usage:\n${Use} ${command.usage.join(`\n${Use} `)}\n`
+
+        if(command.aliases.length >= 1) {
+          let Alias = `${this.client.commandPrefix}${command.aliases[0]}`
+
+          Arguments += `\nUsing Aliases:\n${Alias} ${command.usage.join(`\n${Alias} `)}`
+        }
+        return message.reply(stripIndents`\`\`\`yaml
+        ${Arguments}
+        \`\`\``);
+      }
+      if(command.usage.length == 0) {
+        return message.reply('Please provide an arguments')
+      }
+    }
+
+		if(command.ownerOnly) return;
+
+    if(command.userPermissions) {
+      if(command.length === 1) 
+      {
+        return message.reply(`You need the "${command.userPermissions[0]}" permission for the \`${command.name}\` command to work.`);
+      }
+      if(command.length > 1) {
+        return message.reply(oneLine`
+          You need the following permissions for the \`${command.name}\` command to work:
+          ${command.userPermissions.join(', ')}
+        `);   
+      }
+    }
+
+		if(command.clientPermissions) {
+      if(command.length === 1) 
+      {
+        return message.reply(`I need the "${command.clientPermissions[0]}" permission for the \`${command.name}\` command to work.`);
+      }
+      if(command.length > 1) {
+        return message.reply(oneLine`
+          I need the following permissions for the \`${command.name}\` command to work:
+          ${command.clientPermissions.join(', ')}
+        `);  
+      }
+    }
+
+      if(command) {
+        command.run(
+          { 
+            client: this.client,
+            message, 
+            args, 
+            
+          }
+        )
       }
 
     })
-
-
-    
-
 
 	}
 }
