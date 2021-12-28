98acf89d044dd144683aa82c461d7f44d175be16
Alixy Mizuki <86276165+Alixy-Mizuki@users.noreply.github.com>
2021-12-28 23:51:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let Bot = require('./ext/bot')
 
-
 let bot = new Bot({
-  commandPrefix: '.',
+  commandPrefix: '?',
   case_insensitive: true,
-  owner: '123456789012345678',
+  owner: 'ID',
   intents: [ 'GUILDS', 'GUILD_MESSAGES' ],
   commandFolder: './cmd',
   token: process.env.t
 })
 
 
-bot.on('ready', () => p(bot.user.username+' is alive'))
+bot.on('ready', () => { 
+  console.log(bot.user.username+' is alive')
+})
